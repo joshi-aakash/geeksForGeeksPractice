9e521ab72c0ef1b422e603fa8081b14b6293155a
joshiaakash <joshi.aakash1009@gmail.com>
2022-10-05 19:25:50 -0500

    Added a new class

--- src/BasicPrograms/CheckArmstrongNumber.java
+++ src/BasicPrograms/CheckArmstrongNumber.java
@@ -1,16 +1,44 @@
 package BasicPrograms;
-/*
-A positive integer with digits p, q, r, s…, is known as an Armstrong number of order n if the following
-condition is fulfilled.
-pqrs... = pn + qn + rn + sn +....
-Example:
 
-370 = 3*3*3 + 7*7*7 + 0
-   =  27 + 343 + 0
-   =  370
- */
-public class CheckArmstrongNumber {
-    public static void main(String[] args) {
+public class CheckArmstrongNumber{
+        // Function to print Armstrong
+        // Numbers between two integers
+        static void ArmstrongNum(int l, int h)
+        {
+            for (int j = l + 1; j < h; ++j) {
 
-    }
+                // Calculating number of digits
+                int y = j;
+                int N = 0;
+                while (y != 0) {
+                    y /= 10;
+                    ++N;
+                }
+
+                // Calculating the sum of nth
+                // power of all the digits
+                int sum_power = 0;
+                y = j;
+                while (y != 0) {
+                    int d = y % 10;
+                    sum_power += Math.pow(d, N);
+                    y /= 10;
+                }
+
+                // Checking if the current number
+                // i is equal to the sum of nth
+                // power of all the digits
+                if (sum_power == j)
+                    System.out.print(j + " ");
+            }
+        }
+
+        // The Driver code
+        public static void main(String args[])
+        {
+            int n1 = 50;
+            int n2 = 500;
+            ArmstrongNum(n1, n2);
+            System.out.println();
+        }
 }
